45ea9d7c8161621a2ab182f3f69cc0d7701f514c
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-12-25 22:36:17 -0600

    thermometer functionality

---
+++ public/js/meter.js
@@ -0,0 +1,32 @@
+let amountEl = document.getElementById("amount");
+let counter = document.getElementById("counter");
+const minusBtn = document.querySelector(".decrement");
+const plusBtn = document.querySelector(".increment");
+
+var enzoSavings = localStorage.getItem("enzoSavings");
+counter.textContent = enzoSavings;
+
+const thermometerHeight = function () {
+  amountEl.style.height = counter.innerText * 10 + "%";
+};
+thermometerHeight();
+
+plusBtn.addEventListener("click", function () {
+  console.log("plus clicked");
+  if (enzoSavings < 10) {
+    enzoSavings++;
+    counter.textContent = enzoSavings;
+    localStorage.setItem("enzoSavings", enzoSavings);
+    amountEl.style.height = enzoSavings * 10 + "%";
+  }
+});
+
+minusBtn.addEventListener("click", function () {
+  console.log("minus clicked");
+  if (enzoSavings > 0) {
+    enzoSavings--;
+    counter.textContent = enzoSavings;
+    localStorage.setItem("enzoSavings", enzoSavings);
+    amountEl.style.height = enzoSavings * 10 + "%";
+  }
+});
